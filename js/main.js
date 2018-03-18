3e4214a442eb11a13ad29740d9a4b093822e2cea
Dom Bellettini <dombellettini@gmail.com>
2018-03-18 15:24:27 -0700

    7th styling

--- js/main.js
+++ js/main.js
@@ -13,5 +13,5 @@ function outputCal() {
 	let lifeStyle = (docId('#lifeStyle') * 0.2) + 1;
 
 	let result = docId('#m') ? lifeStyle * (66 + (6.2 * weight) + (12.7 * height) - (6.76 * age)) : lifeStyle * (655.1 + (4.35 * weight) +  (4.7 * height) - (4.7 * age));
-	document.getElementById('output').innerHTML = Math.round(result) + ' calories needed per day to maintain weight.';
+	document.getElementById('output').innerHTML = "<h2 class='finalNum'>" + Math.round(result) + "</h2>  calories needed per day to maintain weight.";
 }
